9dd9c3f7393b77d5c2f2e8470db214d490fab8e0
Rafael Zingano <rafazingano@gmail.com>
2017-11-11 16:49:46 -0200

    Update layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Totvs</title>
-    >
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/jquery.dataTables.min.css') }}" rel="stylesheet">
 </head>
